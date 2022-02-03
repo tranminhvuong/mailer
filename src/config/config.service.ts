9e4fbde424d7dac8a545fdbcc9963d9253944ede
Harsh Makwana <harsh.make1998@gmail.com>
2022-02-04 01:17:26 +0530

    Initial commit

---
+++ src/config/config.service.ts
@@ -0,0 +1,35 @@
+import { config } from 'dotenv';
+import { join } from 'path';
+let envPath;
+if (process.env.NODE_ENV) {
+  envPath = join(__dirname, `../../.${process.env.NODE_ENV}.env`);
+} else {
+  envPath = join(__dirname, '../../../.env');
+}
+config({ path: envPath });
+import { Injectable } from '@nestjs/common';
+
+@Injectable()
+export class ConfigService {
+  private config: { [key: string]: any } = {};
+  constructor() {
+    this.config.rb_url = process.env.RABBITMQ_URL;
+    this.config.token_queue = process.env.RABBITMQ_TOKEN_QUEUE;
+    this.config.main_queue = process.env.RABBITMQ_MAIN_QUEUE;
+    this.config.user_queue = process.env.RABBITMQ_USER_QUEUE;
+    this.config.post_queue = process.env.RABBITMQ_POST_QUEUE;
+    this.config.mailer_queue = process.env.RABBITMQ_MAILER_QUEUE;
+    this.config.redis_host = process.env.REDIS_HOST;
+    this.config.redis_port = process.env.REDIS_PORT;
+    this.config.aws = {
+      accessKeyId: process.env.AWS_ACCESS_KEY,
+      secretAccessKey: process.env.AWS_SECRET_KEY,
+      region: process.env.AWS_REGION,
+    };
+    this.config.sourceEmail = process.env.SOURCE_EMAIL;
+  }
+
+  public get(key: string): any {
+    return this.config[key];
+  }
+}
